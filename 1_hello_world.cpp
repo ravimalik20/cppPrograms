bf2a306a0499c90c1af9e17c9d31a2fe78f4fa86
Ravi Malik <ravimalik2364@gmail.com>
2017-12-01 16:46:46 -0500

    First Commit. Basic hello world.

---
+++ 1_hello_world.cpp
@@ -0,0 +1,14 @@
+/*
+* std::cout : standard output stream.
+* << : 'put to' operator. Writes second argument onto first.
+* std:: signifies that cout is to be found in the standard library namespace.
+*/
+
+#include <iostream>
+
+int main(void)
+{
+	std::cout<<"Hello World\n";
+
+	return 0;
+}
